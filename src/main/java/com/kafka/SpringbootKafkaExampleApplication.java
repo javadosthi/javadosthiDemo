0d2f0b43a37a377654d41e4d4e59ba7975683ee4
balakrishna <mbk.balakrishna90@gmail.com>
2023-02-25 19:52:29 +0530

    added spaces

--- src/main/java/com/kafka/SpringbootKafkaExampleApplication.java
+++ src/main/java/com/kafka/SpringbootKafkaExampleApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootKafkaExampleApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootKafkaExampleApplication.class, args);
+		System.out.println("hello");
 	}
 
 }
